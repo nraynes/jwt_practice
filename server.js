4375b67b346ae36ea6570b53b1f3783b6bc334e7
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2021-10-07 13:54:18 -0500

    Added access tokens

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+
+const app = express();
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'Mark',
+        post: 'Post 1',
+    },
+    {
+        username: 'Josh',
+        post: 'Post 2',
+    },
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => {
+        if (req.user.name === post.username) return post;
+    }));
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = { name: username };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    })
+}
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000...')
+});
